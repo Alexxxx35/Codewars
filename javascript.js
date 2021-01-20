a0c440d4deb76cf5d97b7522c3f86d57f8b3de49
boury_a <boury_a@etna-alternance.net>
2021-01-20 17:20:59 +0100

    js 02

--- javascript.js
+++ javascript.js
@@ -168,6 +168,196 @@ function whoIsPaying(name){
 }
 //-----------------------------------------------------------------------------------------------
 
-function array(arr){
+function array_slicing(arr){
   return arr.split(",").slice(1,-1).join(" ") || null;
+}
+function array_slicing(arr){
+  arr = arr.split(',');
+  
+  if (arr.length < 3) {
+    return null;
+  }
+
+  return arr.slice(1,-1).join(' '); //substring don't affect array data type
+}
+//-----------------------------------------------------------------------------------------------
+
+function logicalCalc(array, op){
+  if (array.length <= 0) {
+    return null;
+  }  
+  //if (array.length === 1 && array[0] === true){return true}
+   // if (array.length === 1 && array[0] === false){return false}
+  res = array[0]
+  for (let i=1;i < array.length;i++){
+    if (typeof array[i] === "boolean"){
+      if(op === "AND"){ res = res && array[i]}
+      if(op === "OR"){ res = res || array[i]}
+      if(op === "XOR"){res = !!(res ^ array[i])}
+    }
+          }
+    return res;
+  }
+
+  function logicalCalc(array, op){
+  if(op === 'AND')return array.every(value => value);
+  else if(op === 'OR')return array.some(value => value);
+  else return !!array.reduce((value, initialvalue) => value ^ initialvalue);
+}
+  //-----------------------------------------------------------------------------------------------
+function swapValues (args) {
+  args.push(args.shift())
+}
+function swapArgs() {
+  return arguments[0].reverse();
+}
+function swapArgs(a) {
+  return a.reverse();
+}
+
+function swapArgs(func) {
+    return function() {
+        func(...Array.from(arguments).reverse());
+    }
+}
+  //-----------------------------------------------------------------------------------------------
+
+function points(games) {
+  var result=0;
+for (let i=0;i<games.length;i++){
+    var x=games[i].split(":")[0]
+    var y=games[i].split(":")[1]
+  if (x>y){result+=3}
+  if (x==y){result+=1}
+  }
+return result
+
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    console.log(typeof game)
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
+
+function points(games) {
+  let total = 0;
+  games.map(function(game){
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
+
+function points(games) {
+  return games.reduce(function(buffer, element){
+    let arraySplit = element.split(':');
+    console.log(arraySplit)
+    return (arraySplit[0] > arraySplit[1]) ? buffer += 3 : (arraySplit[0] < arraySplit[1]) ? buffer : buffer += 1;
+  }, 0);
+}
+
+const points=games=>games.reduce((output,current)=>{
+  console.log(output,current);
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;},0)
+
+  //-----------------------------------------------------------------------------------------------
+function calculator(a,b,sign){
+  if (!Number.isInteger(a) || !Number.isInteger(b)){return "unknown value"}
+  if(sign!="+" && sign!="-" && sign!="*" && sign!="/"){return "unknown value"}
+  if (sign === "+"){return a + b}
+  if (sign === "-"){return a - b}
+  if (sign === "*"){return a * b}
+  if (sign === "/"){return a / b}
+}
+
+function calculator(a,b,sign){
+  if ((typeof a === "number") && (typeof b === "number")) {
+    switch (sign) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    }
+  }
+  return "unknown value";
+}
+
+function calculator(a,b,sign){
+  switch(sign)
+  {
+  case '+': return a + b;
+  case '-': return a - b;
+  case '*': return a * b;
+  case '/': return a / b;
+  default: return "unknown value";
+  }
+
+}
+
+//NE JAMAIS UTILISER
+function calculator(a,b,sign) {
+  try
+    { return eval(a+sign+b); }
+  catch(e)
+    { return "unknown value"; }
+}
+
+//-----------------------------------------------------------------------------------------------
+function milliseconds(x) {
+  if (isNaN(x)) {
+    return 'Not a Number!';
+  }
+  return x * 1000;
+}
+//-----------------------------------------------------------------------------------------------
+function periodIsLate(last, today, cycleLength){
+  var Difference_In_Time = today.getTime() - last.getTime();
+  var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
+  if (Difference_In_Days>cycleLength){
+    return true
+  }
+  return false
+  }
+//-----------------------------------------------------------------------------------------------
+  
+String.prototype.isUpperCase = function() {
+  return String(this) === this.toUpperCase();
+}
+String.prototype.isUpperCase = function () {
+  return !/[a-z]/.test(this);
+};
+
+//-----------------------------------------------------------------------------------------------
+function nameShuffler(str){
+  var r=str.split(" ")
+  r=r[1]+" "+r[0]
+return r
+}
+function nameSuffle(str){
+  return str.split(' ').reverse().join(' ')
+}
+
+//-----------------------------------------------------------------------------------------------
+
+function arrayMadness(a, b) {
+    var r1= a.reduce(function(buffer,element){
+      return buffer+=Math.pow(element,2);
+    },0);
+    var r2= b.reduce(function(buffer,element){
+      return buffer+=Math.pow(element,3);
+    },0);
+    return r1>r2 ? true : false;
 }
